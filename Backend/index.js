570cf7349a664a77ad6c721b77ca28b5541bc12a
madhuri7patel <madhuri7patel@gmail.com>
2022-11-06 19:13:27 +0530

    complted

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const { connection } = require("./config/db.js");
+const express = require("express");
+const { Registerrouter } = require("./routes/register.js");
+const PORT = process.env.PORT;
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Landing page");
+});
+app.use("/register", Registerrouter);
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("madhuri");
+  } catch (err) {
+    console.log(err);
+  }
+});
